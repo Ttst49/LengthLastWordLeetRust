0608dd4cdeb084924a6aadef34dfbfe00fa12430
Thibaut Stachnick <ttst@MBPdeThibaut2.lan>
2024-05-27 19:12:26 +0200

    solve problem

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 fn length_of_last_word(s: String) -> i32 {
     let mut len = 0i32;
-    for as_byte in s.as_bytes().iter().rev() {
+    for &c in s.as_bytes().iter().rev() {
+        if c != b' ' {
+            len+=1
+        }else if len !=0 {
+            break
+        }
         
     }
+    len
 }
 
 
