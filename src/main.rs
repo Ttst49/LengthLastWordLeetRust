6d3b98a121c251731478c95f69d1e9f5105e466a
Thibaut Stachnick <ttst@MacBook-Pro-de-Thibaut-2.local>
2024-05-27 08:56:42 +0200

    end of day

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+fn length_of_last_word(s: String) -> i32 {
+    let mut len = 0i32;
+    for as_byte in s.as_bytes().iter().rev() {
+        
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let sentence = String::from("Luffy is still JoyBoy");
+    length_of_last_word(sentence);
 }
